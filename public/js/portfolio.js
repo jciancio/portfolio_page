fca7bef6ff8cd8f163a4feb8ad351cc3dcfe3beb
jciancio <jciancio7@gmail.com>
2016-06-18 21:07:58 -0400

    major updates

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -1,8 +1,24 @@
 $(document).ready(function(){
-  console.log(document.getElementById('editor'));
   var myTextArea = document.getElementById('editor');
 
   var myCodeMirror = CodeMirror.fromTextArea(myTextArea, {
     lineNumbers: true
   });
+
+  $.fn.goTo = function() {
+    console.log('hey');
+    $('html, body').animate({
+      scrollTop: $(this).offset().top + 'px'
+    }, 'fast');
+    return this;
+  }
+
+  $('#start-learning').on('click', function(e) {
+    e.preventDefault();
+    var editor = $('.code-area');
+    console.log(editor.offset());
+    $('html, body').animate({
+      scrollTop: $(editor).offset().top + 'px'
+    }, 'slow');
+  })
 })
